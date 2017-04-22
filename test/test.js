aa216e89b8236d94bb017fa9ce4bd205f26d26d6
ednagc <ednagc.11@gmail.com>
2017-04-22 20:09:38 +0100

    Añadiendo pruebas mocha

--- test/test.js
+++ test/test.js
@@ -6,4 +6,14 @@ describe("getArea", function() {
     /* There is a white space between consecutive columns */
     result.should.match(/^5000$/);
   })
+  it("must compute the square area correctly", function() {
+    var result = getArea('Square',  { width: 100 });
+    /* There is a white space between consecutive columns */
+    result.should.match(/^10000$/);
+  })
+  it("must compute the rectangle area correctly", function() {
+    var result = getArea('Rectangle',  { width: 100, height: 100 });
+    /* There is a white space between consecutive columns */
+    result.should.match(/^10000$/);
+})
 });
