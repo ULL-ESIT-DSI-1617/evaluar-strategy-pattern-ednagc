89bf315feca6b38f5cd5f52f51871087b8332d5c
ednagc <ednagc.11@gmail.com>
2017-04-22 19:57:22 +0100

    Actualizando y corrigiendo.

--- shapesarea.js
+++ shapesarea.js
@@ -1,14 +1,65 @@
-var shape = require('shape.js');
+"use strict"
+
+class Shape {
+	constructor(options) {
+		this.options_ = options;
+	}
+}
+
+
+class Triangle extends Shape {
+  constructor(options) {
+		super(options);
+	}
+
+	getArea() {
+    let area = .5 * this.options_.width * this.options_.height;
+    return area;
+	}
+}
+
+class Square extends Shape {
+	constructor(options) {
+		super(options);
+	}
+
+	getArea() {
+    let area = Math.pow(this.options_.width, 2);
+		return area;
+	}
+}
+
+
+class Rectangle extends Shape {
+	constructor(options) {
+		super(options);
+	}
+
+	getArea() {
+    let area = this.options_.width * this.options_.height;
+    return area;
+	}
+}
+
+
+
+var figura = {Triangle: 'Triangle',
+            Square: 'Square',
+            Rectangle: 'Rectangle'};
+
 
 module.exports = function(shape, options) {
-  var area = 0;
-  let shapes = shapes;
 
-  try{
-      var clase =  eval(shapes);//Guarda la clase correspondiente a la forma
-      new clase(options);//Creamos la clase del tipo que sea shape
+  let s = figura[shape];
+
+      try{
+          let clase =  eval(s);//Guarda la clase correspondiente a la forma
+          let n_shape = new clase(options);//Creamos la clase del tipo que sea shape
+          let area = n_shape.getArea();
+          return area;
+
+      }
+      catch(err) {
+		      console.log('Desconozco la forma ' + shape);
+	   }
   }
-  catch(err){
-         return 'Desconozco la forma "'+forma+'"';
-  }
-}
