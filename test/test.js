7ec43d30c69362b6b8fd2dfc039819dbe74291f3
ednagc <ednagc.11@gmail.com>
2017-04-22 20:16:54 +0100

    y should

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 var getArea = require("../shapesarea.js");
 
-describe("getArea", function() {
-  it("must compute the triangle area correctly", function() {
-    var result = getArea('Triangle',  { width: 100, height: 100 });
-    /* There is a white space between consecutive columns */
-    result.should.match(/^5000$/);
+  describe("getArea", function() {
+    it("must compute the triangle area correctly", function() {
+      var result = getArea('Triangle',  { width: 100, height: 100 });
+      /* There is a white space between consecutive columns */
+      result.should.match(/^5000$/);
+    })
+    it("must compute the square area correctly", function() {
+      var result = getArea('Square',  { width: 100 });
+      /* There is a white space between consecutive columns */
+      result.should.match(/^10000$/);
+    })
+    it("must compute the rectangle area correctly", function() {
+      var result = getArea('Rectangle',  { width: 100, height: 100 });
+      /* There is a white space between consecutive columns */
+      result.should.match(/^10000$/);
   })
-  it("must compute the square area correctly", function() {
-    var result = getArea('Square',  { width: 100 });
-    /* There is a white space between consecutive columns */
-    result.should.match(/^10000$/);
-  })
-  it("must compute the rectangle area correctly", function() {
-    var result = getArea('Rectangle',  { width: 100, height: 100 });
-    /* There is a white space between consecutive columns */
-    result.should.match(/^10000$/);
-})
 });
